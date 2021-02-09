117e78d0fe10dd97ed15207d033b8200bb28909c
NubeDev <aidan@envrioservices.com.au>
2021-02-10 08:17:01 +1100

    unzip build

---
+++ rubix-wires/systemd/README.md
@@ -0,0 +1,7 @@
+### Commands available
+
+- `bash script.bash -h`
+- `sudo bash script.bash start -service_name=nubeio-rubix-wires.service -u=pi -dir=/home/pi/wires-builds/rubix-wires -data_dir=/data/wires -p=1313`
+- `bash script.bash disable`
+- `bash script.bash enable`
+- `bash script.bash delete`
---
+++ rubix-wires/systemd/ecosystem.config.js
@@ -0,0 +1,22 @@
+const minimist = require('minimist');
+// with pm2 command, argv_index=4
+const argv = minimist([minimist(process.argv.slice(4))['node-args'] || '']);
+const datadir = argv['datadir'] || '/data/rubix-wires/';
+const envFile = argv['envFile'] || '/data/rubix-wires/.env';
+
+module.exports = {
+  apps: [{
+    cwd: __dirname,
+    name: 'rubix-wires',
+    script: 'app.js',
+    args: `--datadir ${datadir} --envFile ${envFile}`,
+    instances: 1, // number process of application. must be `1`
+    exec_mode: 'fork_mode', // must be fork_mode due to pub/sub like `transmitter` cannot work in cluster mode
+    autorestart: true, //auto restart if app crashes
+    max_memory_restart: '200M',
+    env: { NODE_ENV: 'production' },
+    watch: false,
+    out_file: '/dev/null',
+    error_file: '/dev/null',
+  }],
+};
